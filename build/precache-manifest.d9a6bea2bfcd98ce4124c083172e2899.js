02dbfa69a51efc6feb911a9f995f94d67c26bb25
Siddhartha <siddharthasip@gmail.com>
2020-10-26 02:52:09 +0530

    Update to deploy to Netlify

---
+++ build/precache-manifest.d9a6bea2bfcd98ce4124c083172e2899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90c6d6e917d6cdbe607c7494d2c8998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "418a85cdc9cf688046b5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee55bc8f0191e2900891",
+    "url": "/static/js/2.901bba8f.chunk.js"
+  },
+  {
+    "revision": "06ef11e6ae748bc160a74dd4a00e6878",
+    "url": "/static/js/2.901bba8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418a85cdc9cf688046b5",
+    "url": "/static/js/main.c5655cc6.chunk.js"
+  },
+  {
+    "revision": "c01278539cf1fd6986ea",
+    "url": "/static/js/runtime-main.5922fcdd.js"
+  },
+  {
+    "revision": "ffd071a7a5f972e56114400bf2c8ad24",
+    "url": "/static/media/Siddhartha_Profile.ffd071a7.jpg"
+  }
+]);
